c1d9edc2f2690ef326bd8afbe7fc080412cbb224
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-05-29 06:52:01 -0700

    Add health check on treesitter highlights being enabled

--- lua/render-markdown/health.lua
+++ lua/render-markdown/health.lua
@@ -1,19 +1,28 @@
----@param name string
-local function parser_installed(name)
-    local ok = pcall(vim.treesitter.query.parse, name, '')
-    if ok then
-        vim.health.ok(name .. ' parser installed')
-    else
-        vim.health.error(name .. ' parser not found')
-    end
-end
-
 local M = {}
 
 function M.check()
-    vim.health.start('Checking required treesitter parsers')
-    parser_installed('markdown')
-    parser_installed('markdown_inline')
+    vim.health.start('Checking required treesitter parsers & settings')
+    local ok, ts = pcall(require, 'nvim-treesitter.parsers')
+    if not ok then
+        vim.health.error('treesitter is not installed')
+        return
+    end
+    vim.health.ok('treesitter is installed')
+
+    for _, name in ipairs({ 'markdown', 'markdown_inline' }) do
+        if ts.has_parser(name) then
+            vim.health.ok(name .. ' parser installed')
+        else
+            vim.health.error(name .. ' parser not installed')
+        end
+    end
+
+    local highlight = require('nvim-treesitter.configs').get_module('highlight')
+    if highlight.enable then
+        vim.health.ok('treesitter highlights enabled')
+    else
+        vim.health.error('treesitter highlights not enabled')
+    end
 end
 
 return M
