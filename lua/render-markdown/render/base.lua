56878fb59b3eec780e9bc5e12729deea6a2abf4f
Lukas Reineke <lukas@reineke.jp>
2024-09-11 19:13:19 +0900

    fix: heading borders when there is no first heading
    Don't assume that there is always a first level heading when counting
    the indentation level.

--- lua/render-markdown/render/base.lua
+++ lua/render-markdown/render/base.lua
@@ -47,7 +47,7 @@ function Base:indent_virt_line(line)
     if not indent.enabled then
         return line
     end
-    local level = self.info:level() - 1
+    local level = self.info:level()
     if level <= 0 then
         return line
     end
