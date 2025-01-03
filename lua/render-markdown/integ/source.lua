ee2383daedd6b8df829016d143b706df413b4d9f
Alexandre Doucet <alexandre.doucet6@gmail.com>
2025-01-02 21:43:10 -0400

    Improved the code to use treesitter tools to detect the list maker and intert it into the autocomplete suggestion.

--- lua/render-markdown/integ/source.lua
+++ lua/render-markdown/integ/source.lua
@@ -11,6 +11,37 @@ local list_markers = {
 ---@class render.md.Source
 local M = {}
 
+---@private
+---@param row integer
+---@param node TSNode
+---@return string
+function M.list_prefix(row, node)
+    local marker = node:named_child(0)
+    if not marker then
+        return ''
+    end
+
+    local marker_row = marker:range()
+    if marker_row == row then
+        return '' -- Don't run if on the same line, entry should already have a list marker.
+    end
+
+    local start_row = select(1, marker:range())
+    -- Retrieve the line from the buffer
+    local marker_line = vim.api.nvim_buf_get_lines(0, start_row, start_row + 1, false)[1]
+
+    if not marker_line or #marker_line == 0 then
+        return '' -- Return empty if the line is empty or doesn't exist
+    end
+
+    -- Define valid list markers
+    local valid_markers = { ['-'] = true, ['+'] = true, ['*'] = true }
+    local first_char = marker_line:match('%S')
+
+    -- Return corresponding marker if valid, otherwise default to empty string
+    return valid_markers[first_char] and first_char .. ' ' or ''
+end
+
 ---@return boolean
 function M.enabled()
     return manager.is_attached(util.current('buf'))
@@ -38,9 +69,11 @@ function M.items(buf, row, col)
             table.insert(items, M.item(component.raw, component.rendered, nil))
         end
     elseif node:type() == 'list_item' or vim.tbl_contains(list_markers, node:type()) then
+        local last_marker = M.list_prefix(row, node)
+        print(last_marker)
         local checkbox = config.checkbox
-        table.insert(items, M.item('[ ]', checkbox.unchecked.icon, 'unchecked'))
-        table.insert(items, M.item('[x]', checkbox.checked.icon, 'checked'))
+        table.insert(items, M.item(last_marker .. '[ ] ', checkbox.unchecked.icon, 'unchecked'))
+        table.insert(items, M.item(last_marker .. '[x] ', checkbox.checked.icon, 'checked'))
         for name, component in pairs(checkbox.custom) do
             table.insert(items, M.item(component.raw, component.rendered, name))
         end
