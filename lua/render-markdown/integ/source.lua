d7b646f2e6136d963e1bd3abbb9e2ac3fa90837a
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-01-10 12:37:48 -0800

    feat(completions): insert space after marker
    ## Details
    
    Issue: https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/292
    
    Updates the logic of completion text to check if the marker node (for
    both list items and block quotes) ends in a space. If it does not
    then include a space in the completions.
    
    Also adds a call to treesitter parser `parse` method for current row
    to remove various edge cases with treesitter not being up to date after
    text is inserted depending on when completion plugin runs each
    completion source. Should not have a noticeable performance impact since
    we limit range to current row.

--- lua/render-markdown/integ/source.lua
+++ lua/render-markdown/integ/source.lua
@@ -1,3 +1,4 @@
+local Str = require('render-markdown.lib.str')
 local manager = require('render-markdown.manager')
 local state = require('render-markdown.state')
 local util = require('render-markdown.core.util')
@@ -18,7 +19,7 @@ end
 
 ---@return string[]
 function M.trigger_characters()
-    return { '-', '*', '+', '>', ' ', '[' }
+    return { '-', '*', '+', '>', ' ' }
 end
 
 ---@param buf integer
@@ -26,6 +27,13 @@ end
 ---@param col integer
 ---@return lsp.CompletionItem[]?
 function M.items(buf, row, col)
+    local has_parser, parser = pcall(vim.treesitter.get_parser, buf)
+    if not has_parser then
+        return nil
+    end
+
+    -- Parse current row to get up to date node
+    parser:parse({ row, row })
     local node = vim.treesitter.get_node({ bufnr = buf, pos = { row, col } })
     if node == nil then
         return nil
@@ -44,13 +52,14 @@ function M.items(buf, row, col)
     if node:type() == 'block_quote' then
         local quote_row = node:range()
         if quote_row == row then
+            local prefix = M.space_prefix(buf, node)
             for _, component in pairs(config.callout) do
-                table.insert(items, M.item(component.raw, component.rendered, nil))
+                table.insert(items, M.item(prefix .. component.raw, component.rendered, nil))
             end
         end
     elseif node:type() == 'list_item' then
         local checkbox = config.checkbox
-        local prefix = M.list_prefix(row, node)
+        local prefix = M.list_prefix(buf, row, node)
         table.insert(items, M.item(prefix .. '[ ] ', checkbox.unchecked.icon, 'unchecked'))
         table.insert(items, M.item(prefix .. '[x] ', checkbox.checked.icon, 'checked'))
         for name, component in pairs(checkbox.custom) do
@@ -61,20 +70,30 @@ function M.items(buf, row, col)
 end
 
 ---@private
+---@param buf integer
 ---@param row integer
 ---@param node TSNode
 ---@return string
-function M.list_prefix(row, node)
+function M.list_prefix(buf, row, node)
     local marker_node = node:named_child(0)
     if marker_node == nil then
         return ''
     end
     local marker_row = marker_node:range()
     if marker_row == row then
-        return ''
+        return M.space_prefix(buf, marker_node)
     end
     local marker = list_markers[marker_node:type()]
     return marker ~= nil and marker .. ' ' or ''
+end
+
+---@private
+---@param buf integer
+---@param node TSNode
+---@return string
+function M.space_prefix(buf, node)
+    local text = vim.treesitter.get_node_text(node, buf)
+    return Str.spaces('end', text) == 0 and ' ' or ''
 end
 
 ---@private
