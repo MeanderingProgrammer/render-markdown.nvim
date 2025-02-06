f7608829c027139ce8381aa63823df97f7e5a96e
Dan Sully <dsully@users.noreply.github.com>
2025-02-06 14:41:00 -0800

    fix(health): update to not require nvim-treesitter

--- lua/render-markdown/health.lua
+++ lua/render-markdown/health.lua
@@ -21,22 +21,15 @@ function M.check()
         vim.health.error(message)
     end
 
+    M.start('treesitter')
+    M.check_parser('markdown')
+    M.check_parser('markdown_inline')
+
     local latex = state.get(0).latex
     local latex_advice = 'Disable LaTeX support to avoid this warning by setting { latex = { enabled = false } }'
 
-    M.start('nvim-treesitter')
-    local has_treesitter = pcall(require, 'nvim-treesitter')
-    if has_treesitter then
-        vim.health.ok('installed')
-        for _, language in ipairs({ 'markdown', 'markdown_inline' }) do
-            M.check_parser(language)
-            M.check_highlight(language)
-        end
-        if latex.enabled then
-            M.check_parser('latex', latex_advice)
-        end
-    else
-        vim.health.error('not installed')
+    if latex.enabled then
+        M.check_parser('latex', latex_advice)
     end
 
     M.start('icons')
@@ -88,24 +81,14 @@ end
 ---@param language string
 ---@param advice? string
 function M.check_parser(language, advice)
-    local parsers = require('nvim-treesitter.parsers')
-    if parsers.has_parser(language) then
+    local has_parser, _ = pcall(vim.treesitter.get_parser, 0, language, { error = false })
+
+    if has_parser then
         vim.health.ok(language .. ': parser installed')
     elseif advice == nil then
         vim.health.error(language .. ': parser not installed')
     else
         vim.health.warn(language .. ': parser not installed', advice)
-    end
-end
-
----@private
----@param language string
-function M.check_highlight(language)
-    local configs = require('nvim-treesitter.configs')
-    if configs.is_enabled('highlight', language, 0) then
-        vim.health.ok(language .. ': highlight enabled')
-    else
-        vim.health.error(language .. ': highlight not enabled')
     end
 end
 
