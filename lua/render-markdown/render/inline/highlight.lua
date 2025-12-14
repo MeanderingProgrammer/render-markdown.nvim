7a43c8b444fa15b7736b34b248addb951126f723
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-12-14 15:48:18 -0800

    fix: ignore inline highlights that start or end in nested node
    ## Details
    
    Issue: https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/568
    
    Since inline highlights are not actually part of the parser they are
    calculated by finding text inside `==`. This means it picks up text that
    starts / ends in code spans, emphasis blocks, and all other kinds of
    nested inline nodes. As a simple fix for this we now check that the
    start and end are at the top level, meaning the node type is `inline`.

--- lua/render-markdown/render/inline/highlight.lua
+++ lua/render-markdown/render/inline/highlight.lua
@@ -18,26 +18,43 @@ end
 ---@protected
 function Render:run()
     for _, range in ipairs(self.node:find('==[^=]+==')) do
-        -- hide first 2 equal signs
-        self:hide(range[1], range[2], range[2] + 2)
-        -- hide last 2 equal signs
-        self:hide(range[3], range[4] - 2, range[4])
+        local top_level = self:top_level(range[1], range[2])
+            and self:top_level(range[3], range[4])
+        if top_level then
+            -- hide first 2 equal signs
+            self:hide(range[1], range[2], range[2] + 2)
+            -- hide last 2 equal signs
+            self:hide(range[3], range[4] - 2, range[4])
 
-        local custom = self:custom(range, { 0, 2, 0, -2 })
-        local highlight = self.config.highlight
-        if custom then
-            -- hide custom prefix
-            self:hide(range[1], range[2] + 2, range[2] + 2 + #custom.prefix)
-            highlight = custom.highlight
+            local custom = self:custom(range, { 0, 2, 0, -2 })
+            local highlight = self.config.highlight
+            if custom then
+                -- hide custom prefix
+                self:hide(range[1], range[2] + 2, range[2] + 2 + #custom.prefix)
+                highlight = custom.highlight
+            end
+
+            -- highlight contents
+            self.marks:add(self.config, false, range[1], range[2], {
+                end_row = range[3],
+                end_col = range[4],
+                hl_group = highlight,
+            })
         end
-
-        -- highlight contents
-        self.marks:add(self.config, false, range[1], range[2], {
-            end_row = range[3],
-            end_col = range[4],
-            hl_group = highlight,
-        })
     end
+end
+
+---@private
+---@param row integer
+---@param col integer
+---@return boolean
+function Render:top_level(row, col)
+    local node = vim.treesitter.get_node({
+        bufnr = self.context.buf,
+        pos = { row, col },
+        ignore_injections = false,
+    })
+    return node and node:type() == 'inline' or false
 end
 
 ---@private
